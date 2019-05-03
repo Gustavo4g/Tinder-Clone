674d26cc569f9a359812632ca5bbc13e4fd26952
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2019-05-03 14:42:49 +0200

    Add xml more profesional

--- app/src/main/java/com/example/tinder/MainActivity.java
+++ app/src/main/java/com/example/tinder/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tinder;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent i = new Intent(this, Invitacion.class);
+        startActivity(i);
+
     }
 }
