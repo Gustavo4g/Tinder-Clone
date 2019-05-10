c613802ab864d08baefd333bbff7a245642491d5
alejandromonux <alejandro.monux@students.salleurl.edu>
2019-05-10 10:38:55 +0200

    #4  Feat: Creada la clase de Register del Model

---
+++ app/src/main/java/com/example/tinder/Model/Register.java
@@ -0,0 +1,48 @@
+package com.example.tinder.Model;
+
+public class Register {
+    private String email;
+    private String lang;
+    private String login;
+    private String password;
+
+    public Register(String email, String lang, String login, String password) {
+        this.email = email;
+        this.lang = lang;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
