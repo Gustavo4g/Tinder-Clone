32d18f04e58b42454e02dd2fb8b3762e973de583
alejandromonux <alejandro.monux@students.salleurl.edu>
2019-05-10 11:22:47 +0200

    #3 Feat: Creado Invite dentro del model

---
+++ app/src/main/java/com/example/tinder/Model/Invite.java
@@ -0,0 +1,57 @@
+package com.example.tinder.Model;
+
+public class Invite {
+    private boolean accepted;
+    private String createdDate;
+    private int id;
+    private CardOfPeople recieved;
+    private CardOfPeople sent;
+
+    public Invite(boolean accepted, String createdDate, int id, CardOfPeople recieved, CardOfPeople sent) {
+        this.accepted = accepted;
+        this.createdDate = createdDate;
+        this.id = id;
+        this.recieved = recieved;
+        this.sent = sent;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CardOfPeople getRecieved() {
+        return recieved;
+    }
+
+    public void setRecieved(CardOfPeople recieved) {
+        this.recieved = recieved;
+    }
+
+    public CardOfPeople getSent() {
+        return sent;
+    }
+
+    public void setSent(CardOfPeople sent) {
+        this.sent = sent;
+    }
+}
