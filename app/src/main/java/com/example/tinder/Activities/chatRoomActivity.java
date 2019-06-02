fdc61b671d80617cd1afbbc556e2f7109284d906
alejandromonux <alejandro.monux@students.salleurl.edu>
2019-06-02 18:12:30 +0200

    #7 Fix: Añadida implements a chatRoomActivity

--- app/src/main/java/com/example/tinder/Activities/chatRoomActivity.java
+++ app/src/main/java/com/example/tinder/Activities/chatRoomActivity.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.tinder.Interfaces.InviteRequestCallBack;
+import com.example.tinder.Interfaces.PostMessageCallback;
 import com.example.tinder.R;
 
-public class chatRoomActivity extends AppCompatActivity {
+public class chatRoomActivity extends AppCompatActivity implements PostMessageCallback, InviteRequestCallBack {
     private RecyclerView profileRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_chat);
+
+    }
+
+    @Override
+    public void onProfileInviteSuccess() {
+
+    }
+
+    @Override
+    public void onProfileInviteFailed(String reason) {
+
+    }
+
+    @Override
+    public void onPostMessageSuccess() {
+
+    }
+
+    @Override
+    public void onPostMessageFailed(String reason) {
 
     }
 }
