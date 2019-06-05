58814491decc2bb75314914a4cb4c3013fd642fe
MarcAynes <marcaynes2@gmail.com>
2019-06-05 19:03:37 +0200

    merge

--- app/src/main/java/com/example/tinder/Activities/chatRoomActivity.java
+++ app/src/main/java/com/example/tinder/Activities/chatRoomActivity.java
@@ -1,15 +1,21 @@
 package com.example.tinder.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.tinder.Connection.TinderManager;
 import com.example.tinder.GenericCallback;
 import com.example.tinder.Model.Message;
 import com.example.tinder.R;
 
+import java.util.Date;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,6 +27,10 @@ import java.util.Locale;
 
 public class chatRoomActivity extends AppCompatActivity implements GenericCallback {
     private RecyclerView recycle;
+    private ImageView perfil;
+    private TextView name;
+    private Button button;
+    private TextInputEditText tersto;
     private ArrayList<com.example.tinder.Model.Message> messages;
     private float id;
     @Override
@@ -34,7 +44,39 @@ public class chatRoomActivity extends AppCompatActivity implements GenericCallba
         for (Message fer : messages){
             Log.d("Pepe",fer.getMessage());
         }
+
         recycle = findViewById(R.id.recyclerView);
+        perfil = findViewById(R.id.personalImage);
+        name = findViewById(R.id.nameChat);
+        tersto = findViewById(R.id.introducion);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> send());
+
+        TinderManager.getInstance().getMessages(id, new GenericCallback() {
+            @Override
+            public void onSuccess(Object data) {
+
+            }
+
+            @Override
+            public void onFailure(Object data) {
+
+            }
+        }, );
+
+    }
+
+    private void send(){
+        if (tersto.getText() != null) {
+            String mensaje = tersto.getText().toString();
+            Date date= new Date();
+            long time = date.getTime();
+            Timestamp ts = new Timestamp(time);
+            //Message m = new Message();
+
+            //TinderManager.getInstance().postMessages();
+        }
     }
 
 
